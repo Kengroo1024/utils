4ced906dc600da886179d4557c92a53c01855be9
Kengroo1024 <dz6xbr0ztga@qq.com>
2025-03-10 13:37:19 +0800

    modified xspfgen.py

--- xspfgen.py
+++ xspfgen.py
@@ -3,17 +3,19 @@
 import argparse
 import json
 import subprocess
-from pathlib import Path
 import time
-from math import log10
 import xml.etree.ElementTree as ET
+from math import log10
+from pathlib import Path
 from urllib.parse import quote
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-f", nargs="?", type=Path)
 parser.add_argument("-p", nargs="?", default="")
-parser.add_argument("path", nargs="?", default=Path("."),type=Path)
+parser.add_argument("path", nargs="?", default=Path("."), type=Path)
+parser.add_argument("-m", action="store_True", nargs="?",
+                    help="if the xspf file stores the metadata")
 
 audio_format = (".mp3", ".flac", ".ogg", ".m4a", ".ape", ".acc", ".wav")
 
@@ -42,54 +44,56 @@ def main():
                 toal += 1
 
     lm = int(log10(toal) + 1)
-    print("\033[?25l")
 
     start = int(time.time())
 
     for supdir, subdir, files in par.path.walk():
         for file in files:
             if file.endswith(audio_format):
-                m = supdir / file
-                p = subprocess.run(
-                    [
-                        "ffprobe",
-                        "-hide_banner",
-                        "-v",
-                        "quiet",
-                        "-print_format",
-                        "json",
-                        "-show_entries",
-                        "format_tags=title,artist,album,track",
-                        "-show_entries",
-                        "format=duration",
-                        "-i",
-                        m,
-                    ],
-                    stdout=subprocess.PIPE,
-                    encoding="utf-8",
-                )
-                format_dict = json.loads(p.stdout)["format"]
-                track = ET.SubElement(track_list, f"{{{NS}}}track")
-                ET.SubElement(track, f"{{{NS}}}location").text = par.p + quote(m.as_posix())
-                ET.SubElement(track, f"{{{NS}}}title").text = format_dict["tags"][
-                    "title"
-                ]
-                ET.SubElement(track, f"{{{NS}}}creator").text = format_dict["tags"][
-                    "artist"
-                ]
-                ET.SubElement(track, f"{{{NS}}}album").text = format_dict["tags"][
-                    "album"
-                ]
-                try:
-                    ET.SubElement(track, f"{{{NS}}}trackNum").text = format_dict[
-                        "tags"
-                    ]["track"]
-                except KeyError:
-                    pass
-                ET.SubElement(track, f"{{{NS}}}duration").text = str(
-                    int(float(format_dict["duration"]) * 1000)
-                )
+                if par.m:
+                    m = supdir / file
+                    p = subprocess.run(
+                        [
+                            "ffprobe",
+                            "-hide_banner",
+                            "-v",
+                            "quiet",
+                            "-print_format",
+                            "json",
+                            "-show_entries",
+                            "format_tags=title,artist,album,track",
+                            "-show_entries",
+                            "format=duration",
+                            "-i",
+                            m,
+                        ],
+                        stdout=subprocess.PIPE,
+                        encoding="utf-8",
+                    )
+                    format_dict = json.loads(p.stdout)["format"]
+                    track = ET.SubElement(track_list, f"{{{NS}}}track")
 
+                    ET.SubElement(track, f"{{{NS}}}title").text = format_dict["tags"][
+                        "title"
+                    ]
+                    ET.SubElement(track, f"{{{NS}}}creator").text = format_dict["tags"][
+                        "artist"
+                    ]
+                    ET.SubElement(track, f"{{{NS}}}album").text = format_dict["tags"][
+                        "album"
+                    ]
+                    try:
+                        ET.SubElement(track, f"{{{NS}}}trackNum").text = format_dict[
+                            "tags"
+                        ]["track"]
+                    except KeyError:
+                        pass
+                    ET.SubElement(track, f"{{{NS}}}duration").text = str(
+                        int(float(format_dict["duration"]) * 1000)
+                    )
+
+                ET.SubElement(
+                    track, f"{{{NS}}}location").text = par.p + quote(m.as_posix())
                 # 进度条更新
                 i += 1
                 shiwei, gewei = divmod((i * 100) // toal, 10)
@@ -118,6 +122,7 @@ def main():
 
 
 if __name__ == "__main__":
+    print("\033[?25l")
     try:
         main()
     finally:
